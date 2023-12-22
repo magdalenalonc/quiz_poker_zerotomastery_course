16eaa6385b06480182322266874b33dec36e5008
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-12-22 23:40:29 +0100

    Create layout of HomePage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../data/questions_list.dart';
+import '../models/question.dart';
+import '../widgets/question_item.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -9,8 +13,14 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Quiz Poker"),
       ),
-      body: const Center(
-        child: Text("Welcome to Quiz Poker!"),
+      body: ListView.builder(
+        itemCount: questions.length,
+        itemBuilder: (BuildContext context, int index) {
+          for (Question question in questions) {
+            return QuestionItem(question: question);
+          }
+          return null;
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showSnackBar(context),

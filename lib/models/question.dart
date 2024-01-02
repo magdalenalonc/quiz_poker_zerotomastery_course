68c11142a334f7d1e90d918962824a27377e9d9f
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-01-02 22:34:23 +0100

    Refactor Question and questions list

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -1,8 +1,17 @@
 enum Category { sport, music, movie, animals, other }
 
 class Question {
-  Question({required this.category, required this.questionText});
+  Question({
+    required this.category,
+    required this.questionText,
+    required this.hint1,
+    required this.hint2,
+    required this.solution,
+  });
 
   final Category category;
   final String questionText;
+  final String hint1;
+  final String hint2;
+  final String solution;
 }

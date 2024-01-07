2c47d14206f74d2b218f580311bc3e88e916e619
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-01-07 21:17:36 +0100

    Add navigation & IndexedStack in HomePage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,13 +1,8 @@
-import 'dart:io' show Platform;
-import 'dart:math';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:quiz_poker_zerotomastery_course/screens/question_detail_page.dart';
 
-import '../data/questions_list.dart';
-import '../models/question.dart';
-import '../widgets/question_item.dart';
+import 'question_form_page.dart';
+import 'questions_list_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -32,23 +27,6 @@ class _HomePageState extends State<HomePage> {
       );
     });
   }*/
-
-  void _goToQuestionDetail(Question question) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => QuestionDetailPage(question: question),
-      ),
-    );
-  }
-
-  void _goToRandomQuestionDetail() {
-    if (questions.isNotEmpty) {
-      Random random = Random();
-      int randomIndex = random.nextInt(questions.length);
-      _goToQuestionDetail(questions[randomIndex]);
-    }
-  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -94,40 +72,15 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscapeMode =
-        MediaQuery.of(context).size.width >= MediaQuery.of(context).size.height;
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Quiz Poker"),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: const [
+          QuestionsListPage(),
+          QuestionFormPage(),
+        ],
       ),
-      body: ListView.builder(
-        physics: const BouncingScrollPhysics(),
-        scrollDirection: isLandscapeMode ? Axis.horizontal : Axis.vertical,
-        itemCount: questions.length,
-        itemBuilder: (BuildContext context, int index) {
-          return QuestionItem(
-            question: questions[index],
-            onTap: () => _goToQuestionDetail(questions[index]),
-          );
-        },
-      ),
-      /* Other approach:
-            body: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Column(
-                children: [
-                  for (Question question in questions) QuestionItem(question: question),
-                ],
-              ),
-            )*/
       bottomNavigationBar: _buildBottomNavigationBar(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _goToRandomQuestionDetail,
-        splashColor: Theme.of(context).colorScheme.secondary,
-        tooltip: "Random question",
-        child: const Icon(Icons.question_mark),
-      ),
     );
   }
 }
